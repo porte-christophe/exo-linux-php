b0e7117f3fe47ab91040418064bc783daa5a5191
Christophe <porte17christophe69@gmail.com>
2026-09-07 17:11:54 +0200

    exo17

--- exos/exo17.php
+++ exos/exo17.php
@@ -1,6 +1,19 @@
 <?php
 	$mot = 'ordinateur';
 	$lettre = 'r';
+	$res =[];
 
 	$motMinus = strtolower($mot);
+
+	for ($i=0; $i < strlen($motMinus) ; $i++) { 
+		if ($lettre == substr($motMinus, $i,1)) {
+			array_push($res, $i+1);
+		}
+	}
+	if ($res != []) {
+		echo "position(s) " . implode(" et ", $res) . " dans le mot: " . $mot;
+	} else {
+		echo "lettre non presente";
+	}
+	
 ?>
